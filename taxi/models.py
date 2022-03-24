e3f3b84bafe8d836b85748b7a62f97f6a8a3211d
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-03-24 21:15:30 +0200

    Fixed models.py

--- taxi/models.py
+++ taxi/models.py
@@ -8,7 +8,7 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
         verbose_name = "driver"
