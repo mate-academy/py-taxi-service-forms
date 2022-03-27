00cdbc8d165edac5cea51706bd1fe98daa0f5965
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-03-27 21:54:12 +0300

    Class-based views

---
+++ taxi/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Driver, Car, Manufacturer
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+    }
+
+    return render(request, "taxi/index.html", context=context)
