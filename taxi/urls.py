2593592d8039d8530c09477a0e6f99d7579e092a
Taras Nester <tarasnester414@gmail.com>
2022-07-03 21:48:53 +0300

    flake8 fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views import (
     index,
     ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
     CarListView,
     CarDetailView,
     CarCreateView,
@@ -12,28 +15,81 @@ from .views import (
     DriverDetailView,
     DriverCreateView,
     DriverDeleteView,
-    DriverUpdateView, ManufacturerCreateView, ManufacturerUpdateView,
-    ManufacturerDeleteView
+    DriverUpdateView,
 )
-
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
-    path("manufacturers/create", ManufacturerCreateView.as_view(), name="manufacturer-form"),
-    path("manufacturers/<int:pk>/update/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
-    path("manufacturers/<int:pk>/delete/", ManufacturerUpdateView.as_view(), name="manufacturer-form"),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create/", CarCreateView.as_view(), name="car-form"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-form"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/create", DriverCreateView.as_view(), name="driver-form"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
-    path("drivers/<int:pk>/update", DriverUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
-
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path(
+        "manufacturers/create",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-form"
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete"
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-form"
+    ),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "cars/create/",
+        CarCreateView.as_view(),
+        name="car-form"
+    ),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-form"
+    ),
+    path(
+        "cars/<int:pk>/delete/",
+        CarDeleteView.as_view(),
+        name="car-delete"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "drivers/create",
+        DriverCreateView.as_view(),
+        name="driver-form"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path(
+        "drivers/<int:pk>/update",
+        DriverUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
 ]
 
 app_name = "taxi"
