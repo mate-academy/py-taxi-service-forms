423a3a2160a269d1e272c89319219e1d3cff0732
Taras Nester <tarasnester414@gmail.com>
2022-07-06 08:54:45 +0300

    fix validatator

--- taxi/forms.py
+++ taxi/forms.py
@@ -7,7 +7,7 @@ from taxi.models import Driver
 def validate_license_number(number):
     if not len(number) == 8:
         raise ValidationError("License number should consist of 8 characters")
-    elif not number[:3].isupper() and not number[:3].isalpha():
+    elif not number[:3].isupper() or not number[:3].isalpha():
         raise ValidationError("First 3 characters should be uppercase letters")
     elif not number[3:].isdigit():
         raise ValidationError("Last 5 characters should be digits")
