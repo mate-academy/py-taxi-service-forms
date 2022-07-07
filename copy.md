1b7c6b3cc052a82b593718817088065cc6b820c6
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-07-07 17:12:57 +0300

    'fix_blank'

--- copy.md
+++
@@ -1,9 +0,0 @@
-7. On the car detail page add button `Assign me to this car`. This button adds 
-current user to car drivers. When current user is already a driver of this car, 
-there should be `Delete me from this car` button that deletes user from car drivers.
-
-The car detail page should look like this:
-![image](https://mate-academy-images.s3.eu-central-1.amazonaws.com/django-forms-1.png)
-![image](https://mate-academy-images.s3.eu-central-1.amazonaws.com/django-forms-2.png)
-
-NOTE: Attach screenshots of all created or modified pages to pull request. It's important to attach images not links to them.
--- taxi/urls.py
+++ taxi/urls.py
@@ -5,15 +5,32 @@ from .views import (
     DriverDetailView, ManufacturerListView, CarCreateView,
     CarUpdateView, CarDeleteView, ManufacturerCreateView,
     ManufacturerUpdateView, ManufacturerDeleteView, DriverCreateView,
-    DriverUpdateView, DriverDeleteView, car_add_driver_view, car_delete_driver_view
+    DriverUpdateView, DriverDeleteView, car_add_driver_view,
+    car_delete_driver_view
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
-    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
-    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create"
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update"
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete"
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
@@ -29,10 +46,22 @@ urlpatterns = [
         car_delete_driver_view,
         name="car-delete-driver"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/update/", DriverUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
 ]
 
 app_name = "taxi"
