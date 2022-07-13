691f5889dfff0f338e92c3fd36689ff0dbddf94f
Flashmobber <ufinaid@gmail.com>
2022-07-13 19:13:34 +0300

    UserCreationForm implemented

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,21 +1,23 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
 
 from taxi.models import Driver
 
 
-class DriverCreateForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+class DriverCreateForm(UserCreationForm):
     license_number = forms.CharField(required=True,
                                      min_length=8,
                                      max_length=8,
+                                     help_text="License format: AAA00000",
                                      validators=[
                                          RegexValidator("^[A-Z]{3}\d{5}")])
 
     class Meta:
         model = Driver
-        fields = ["username", "password", "first_name", "last_name", "email",
-                  "license_number"]
+        fields = UserCreationForm.Meta.fields + ("first_name",
+                                                 "last_name",
+                                                 "license_number")
 
 
 class DriverUpdateForm(forms.ModelForm):
