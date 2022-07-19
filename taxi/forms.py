800a08978d267aa815f9f3b5e1b360edf22b497e
Elena <lenasharabura@gmail.com>
2022-07-19 17:45:13 +0200

    add license validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,11 +6,24 @@ from django.forms import CheckboxSelectMultiple
 from taxi.models import Driver, Car
 
 
+def validate_license_number(license_number):
+    if len(license_number) != 8:
+        raise forms.ValidationError("The license number consists of 8 characters!")
+    if not license_number[:3].isupper():
+        raise forms.ValidationError("The first 3 characters of license number must be a capital letters!")
+    if not license_number[3:].isdigit():
+        raise forms.ValidationError("The last 5 characters of license number must be a numbers!")
+    return license_number
+
+
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "license_number", "email",)
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
 
 
 class LicenseNumberForm(forms.ModelForm):
@@ -20,14 +33,7 @@ class LicenseNumberForm(forms.ModelForm):
         fields = ("license_number",)
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise forms.ValidationError("The license number consists of 8 characters!")
-        if not license_number[:3].isupper():
-            raise forms.ValidationError("The first 3 characters of license number must be a capital letters!")
-        if not license_number[3:].isdigit():
-            raise forms.ValidationError("The last 5 characters of license number must be a numbers!")
-        return license_number
+        return validate_license_number(self.cleaned_data["license_number"])
 
 
 class CarForm(forms.ModelForm):
