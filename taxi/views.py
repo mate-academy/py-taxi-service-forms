6a451b41f2582f92bfd914d1969e3bb80366848b
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-09-06 16:58:00 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -34,6 +36,42 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 2
 
 
+class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Manufacturer
+    fields = ["name"]  # !!!
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_confirm_delete.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+# def manufacturer_create_view(request):
+#     print("request:", request)
+#     context = {}
+#     print("request.POST:", request.POST)
+#     form = ManufacturerForm(request.POST or None)
+#     print("form", form)
+#     print("request.POST:", request.POST)
+#
+#     if form.is_valid():
+#         print("form.is_valid:", form.is_valid)
+#         print("form.cleaned_data:", form.cleaned_data)
+#         form.save()
+#         print('reverse("taxi:manufacturer-list"):', reverse("taxi:manufacturer-list"))
+#         return HttpResponseRedirect(reverse("taxi:manufacturer-list"))
+#
+#     context["form"] = form
+#     return render(request, "taxi/manufacturer_form.html", context=context)
+
+
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 2
@@ -44,6 +82,24 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
 
 
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+
+
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+
+
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    template_name = "taxi/ car_confirm_delete.html"
+    success_url = reverse_lazy("taxi:car-list")
+
+
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 2
@@ -52,3 +108,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+    def get_slug_field(self):
+        return 'username'
+
