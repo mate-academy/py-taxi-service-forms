03af3390a7874878ad9b73b2c83d3b03551ce8ee
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-07 18:53:45 +0200

    Refactoring

--- taxi/urls.py
+++ taxi/urls.py
@@ -42,7 +42,6 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
