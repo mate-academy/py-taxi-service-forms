c30f1e77df0baf3de665a41603e7b4fa5caf80d4
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-11-11 19:19:57 +0200

    'py_taxi_service_forms_5'

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.http import request
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
