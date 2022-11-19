8769c16d1446ac39d8296cbc63679a481a0a7b22
AndriyKy <akrechovetskiy@gmail.com>
2022-11-19 18:06:50 +0200

    Solution2

--- taxi/urls.py
+++ taxi/urls.py
@@ -25,28 +25,31 @@ urlpatterns = [
     path(
         "manufacturers/create/",
         ManufacturerCreateView.as_view(),
-        name="manufacturer-list-create"
+        name="manufacturer-create"
     ),
     path(
         "manufacturers/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
-        name="manufacturer-list-update"
+        name="manufacturer-update"
     ),
     path(
         "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
-        name="manufacturer-list-delete"
+        name="manufacturer-delete"
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/create/", CarCreateView.as_view(), name="car-list-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-list-update"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-update"
+    ),
     path(
         "cars/<int:pk>/delete/",
         CarDeleteView.as_view(),
-        name="car-list-delete"
+        name="car-delete"
     ),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
