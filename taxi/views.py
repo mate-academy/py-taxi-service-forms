f8deb4238c38fb268ea6c735a9cf27314eef96fb
eugene <bantyc8@gmail.com>
2022-11-21 18:54:39 +0200

    new solution with work button

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
