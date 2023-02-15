409b618f76666654a32828474007034441fadaf0
Serhii Kovalenko <27kovalenko@gmail.com>
2023-02-15 19:02:47 +0200

    Fixed error

--- taxi/views.py
+++ taxi/views.py
@@ -55,7 +55,7 @@ class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
