e28530259039828396e97e02191c135b1f0359b8
Zoriana Dvulit <zdvulit@ukr.net>
2023-03-25 13:57:36 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -28,12 +28,12 @@ urlpatterns = [
         name="manufacturer-create",
     ),
     path(
-        "manufacturers/<int:pk>/update",
+        "manufacturers/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
         name="manufacturer-update",
     ),
     path(
-        "manufacturers/<int:pk>/delete",
+        "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
@@ -52,12 +52,12 @@ urlpatterns = [
     ),
 
     path(
-        "cars/<int:pk>/update",
+        "cars/<int:pk>/update/",
         CarUpdateView.as_view(),
         name="car-update",
     ),
     path(
-        "cars/<int:pk>/delete",
+        "cars/<int:pk>/delete/",
         CarDeleteView.as_view(),
         name="car-delete",
     ),
