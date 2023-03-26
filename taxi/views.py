edcff5f3e9d73571ac1e5fe0a04bdfc81c84d479
Yulia <Mala>
2023-03-26 18:07:10 +0300

    cancel option added

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import request
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Driver, Car, Manufacturer
