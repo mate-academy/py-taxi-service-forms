671673d9d668c0957da9f33c6e52e270073f0798
Yulia <Mala>
2023-03-28 20:47:02 +0300

    Redundant template names killed

--- taxi/views.py
+++ taxi/views.py
@@ -70,7 +70,6 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_confirm_delete.html"
 
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
@@ -89,4 +88,3 @@ class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
     fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_confirm_delete.html"
