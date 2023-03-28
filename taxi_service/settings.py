49c86c9efbaf429f55edfbbc9e03d80ef452b9cd
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-28 17:52:55 +0300

    delete redundant comment

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "crispy_bootstrap4",
     "crispy_forms",
     "debug_toolbar",
     "taxi",
