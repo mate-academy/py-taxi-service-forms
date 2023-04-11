7871c018e960be7a366ddb00fed2f461739dc7fd
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-11 08:51:46 +0300

    Implement all CRUD operations for Car model

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,9 +4,12 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarCreateView,
+    CarUpdateView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ManufacturerCreateView, CarDeleteView
 )
 
 urlpatterns = [
@@ -16,8 +19,12 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
