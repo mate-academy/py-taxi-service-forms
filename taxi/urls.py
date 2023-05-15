40f8153521272980b9cfb9d70ed8cc0a0565c4e9
Artem Motrunich <motrunich2013@gmail.com>
2023-05-15 18:46:32 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import (
     index,
@@ -7,21 +7,53 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    CarCreateView,
+    CarUpdateView,
+    CarDeleteView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
+
+car_urls = [
+    path("", CarListView.as_view(), name="car-list"),
+    path("<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("create/", CarCreateView.as_view(), name="car-create"),
+    path("<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+]
+
+manufacturer_urls = [
+    path("", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path(
+        "create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create"
+    ),
+    path(
+        "<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update"
+    ),
+    path(
+        "<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete"
+    ),
+]
+
+driver_urls = [
+    path("", DriverListView.as_view(), name="driver-list"),
+    path(
+        "<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+]
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("manufacturers/", include(manufacturer_urls)),
+    path("cars/", include(car_urls)),
+    path("drivers/", include(driver_urls)),
 ]
 
 app_name = "taxi"
