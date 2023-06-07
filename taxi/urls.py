d0656a54a09f69c7598a790991c9539f29614092
V Borovets <vborovets0@gmail.com>
2023-06-07 19:47:44 +0300

    start: car(create), manufacturer (CUD)

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,9 +4,14 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarCreateView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
+
 )
 
 urlpatterns = [
@@ -16,12 +21,18 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+
+    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturer-form/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturer-form/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+
 ]
 
 app_name = "taxi"
