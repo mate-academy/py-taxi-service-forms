46186519253b6281049286b4778ea9e2818fa29e
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-06-22 16:07:47 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,8 +6,13 @@ from .views import (
     CarDetailView,
     DriverListView,
     DriverDetailView,
-    ManufacturerListView, CarCreateView, CarUpdateView, CarDeleteView,
-    ManufacturerCreateView, ManufacturerUpdateView, ManufacturerDeleteView,
+    ManufacturerListView,
+    CarCreateView,
+    CarUpdateView,
+    CarDeleteView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
