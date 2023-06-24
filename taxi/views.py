51ed69691edf67c6e146ff66b03733ce3d9b7a8b
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-06-25 01:37:00 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -49,7 +49,7 @@ class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
-    success_url = reverse_lazy("taxi:ManufacturerDeleteView")
+    success_url = reverse_lazy("taxi:manufacturer-list")
     template_name = "taxi/manufacturer_confirm_delete.html"
 
 
