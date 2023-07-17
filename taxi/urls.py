bf3bebdc0f9e1135cd82343ceca5c4fe42d0558b
maccarets <maccarets@gmail.com>
2023-07-17 10:47:55 +0300

    Done

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,7 +9,10 @@ from .views import (
     ManufacturerListView,
     CarCreateView,
     CarUpdateView,
-    ManufacturerCreateView, ManufacturerUpdateView, CarDeleteView, ManufacturerDeleteView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    CarDeleteView,
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
@@ -23,18 +26,37 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
     ),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path(
-        "manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"
+        "cars/create/",
+        CarCreateView.as_view(),
+        name="car-create"
+    ),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create"),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-update"),
+    path(
+        "cars/<int:pk>/delete/",
+        CarDeleteView.as_view(),
+        name="car-delete"),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update"
     ),
 
     path(
-        "manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete"
     ),
 ]
 
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "taxi",
-    'crispy_forms',
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
