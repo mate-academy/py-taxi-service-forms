871720e6c1a7e548630c46ca4e6f4159abf0f9a4
maccarets <maccarets@gmail.com>
2023-07-19 16:31:38 +0300

    Done

--- taxi/urls.py
+++ taxi/urls.py
@@ -38,21 +38,23 @@ urlpatterns = [
     path(
         "manufacturers/create/",
         ManufacturerCreateView.as_view(),
-        name="manufacturer-create"),
+        name="manufacturer-create"
+    ),
     path(
         "cars/<int:pk>/update/",
         CarUpdateView.as_view(),
-        name="car-update"),
+        name="car-update"
+    ),
     path(
         "cars/<int:pk>/delete/",
         CarDeleteView.as_view(),
-        name="car-delete"),
+        name="car-delete"
+    ),
     path(
         "manufacturers/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
         name="manufacturer-update"
     ),
-
     path(
         "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
