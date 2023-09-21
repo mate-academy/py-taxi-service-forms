63c07e7ccee96142f0cfe160b8c4a2f5275ef9a5
Serhii-Leonenko <leonenko3149@gmail.com>
2023-09-21 19:37:08 +0300

    added changes

--- taxi/views.py
+++ taxi/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 
-from .models import Driver, Car, Manufacturer
+from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
     """View function for the home page of the site."""
 
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-
     context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
     }
 
     return render(request, "taxi/index.html", context=context)
