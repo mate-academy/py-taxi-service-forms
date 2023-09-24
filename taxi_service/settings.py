280ec936b00d306e6a1de89879d0437d45c32093
OlegatorLE <oleh.travin@gmail.com>
2023-09-24 12:26:56 +0300

    Solution 1.2

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "crispy_bootstrap4",
-    "crispy_forms",
     "taxi",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
