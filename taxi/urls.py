5cf80126e6b698b309f5e3f8c75a72be394c0f33
Max Katkalov <makskatkalov@gmail.com>
2023-09-25 14:51:46 +0300

    solution implemented

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import (
     index,
@@ -7,17 +7,61 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    CarCreateView,
+    CarUpdateView,
+    CarDeleteView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
 
-urlpatterns = [
-    path("", index, name="index"),
+cars_urlpatterns = [
+    path("", CarListView.as_view(), name="car-list"),
+    path("create/", CarCreateView.as_view(), name="car-create"),
     path(
-        "manufacturers/",
+        "<int:pk>/",
+        include(
+            [
+                path("", CarDetailView.as_view(), name="car-detail"),
+                path("update/", CarUpdateView.as_view(), name="car-update"),
+                path("delete/", CarDeleteView.as_view(), name="car-delete"),
+            ]
+        ),
+    ),
+]
+
+manufacturers_urlspatterns = [
+    path(
+        "",
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path(
+        "create/", ManufacturerCreateView.as_view(), name="manufacturer-create"
+    ),
+    path(
+        "<int:pk>/",
+        include(
+            [
+                path(
+                    "update/",
+                    ManufacturerUpdateView.as_view(),
+                    name="manufacturer-update",
+                ),
+                path(
+                    "delete/",
+                    ManufacturerDeleteView.as_view(),
+                    name="manufacturer-delete",
+                ),
+            ]
+        ),
+    ),
+]
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("cars/", include(cars_urlpatterns)),
+    path("manufacturers/", include(manufacturers_urlspatterns)),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
