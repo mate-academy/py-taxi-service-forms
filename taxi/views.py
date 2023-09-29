a7bd99b81010cbb6f6b1ee59493568fcf9ef072b
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-09-29 09:56:01 +0300

    update views

--- taxi/views.py
+++ taxi/views.py
@@ -30,8 +30,6 @@ def index(request):
 
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
@@ -69,7 +67,6 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
-    template_name = "taxi/car_confirm_delete.html"
     success_url = reverse_lazy("taxi:car-list")
 
 
