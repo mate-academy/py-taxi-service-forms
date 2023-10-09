d30246c6b16957ae44dc29b39a93fb17f84afdf0
anna-lybid <anna.lybid@gmail.com>
2023-10-09 23:14:23 +0300

    Removed redundant .all() methods

--- taxi/views.py
+++ taxi/views.py
@@ -54,7 +54,7 @@ class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
@@ -85,6 +85,6 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related(
+    queryset = Driver.objects.prefetch_related(
         "cars__manufacturer"
     )
