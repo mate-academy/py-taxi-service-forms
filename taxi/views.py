e7605c742204c728c527ff0781914aaee4b10201
Yuliia <verbikyullya@gmail.com>
2023-10-13 00:48:16 +0300

    /

--- taxi/views.py
+++ taxi/views.py
@@ -51,7 +51,7 @@ class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
-    success_url = reverse_lazy("catalog:literary-format-list")
+    success_url = reverse_lazy("taxi:manufacturer-list")
     template_name = "taxi/manufacturer_confirm_delete.html"
 
 
