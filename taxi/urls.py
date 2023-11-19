794c561266c008200b63ff79a31bd9a9cf7d5e50
Andrii Ishchenko <iscenkoa@gmail.com>
2023-11-19 14:08:51 +0200

    sorry stupid mistake

--- taxi/urls.py
+++ taxi/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path(
-        "cars/create",
+        "cars/create/",
         CarCreateView.as_view(),
         name="car-create"
     ),
