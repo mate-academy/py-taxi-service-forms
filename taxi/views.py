0b1b73223512697cf095dbd1d82f7c362c8767c6
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-11-20 13:25:09 +0200

    Deleted fields in DeleteView

--- taxi/views.py
+++ taxi/views.py
@@ -49,7 +49,6 @@ class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
-    fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
@@ -73,7 +72,6 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
-    fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
 
 
