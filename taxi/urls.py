705529e8aaf05beb4b5b4b0a117f1458501be736
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-01-08 21:23:11 +0200

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -10,7 +10,6 @@ from .views import (
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
-    ManufacturerDetailView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
