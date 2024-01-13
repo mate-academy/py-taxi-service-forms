9822305c49666430aa6a33a084cea566f5d1d353
DanSheremeta <danylo3002@gmail.com>
2024-01-13 23:59:30 +0200

    Fixed flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,7 +15,6 @@ from .views import (
     ManufacturerDeleteView,
 )
 
-
 urlpatterns = [
     path("", index, name="index"),
     path(
@@ -23,10 +22,21 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
-    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
-    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
-
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create"
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update"
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete"
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
