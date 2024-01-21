938308c9486d8f0c841e08c3a23c4fb0c3da3c44
Vlad Krit <kritvladislav98@gmail.com>
2024-01-21 18:27:00 +0200

    solution_py-taxi-service-forms

--- taxi_service/wsgi.py
+++ taxi_service/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi_service.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi_service.settings")
 
 application = get_wsgi_application()
