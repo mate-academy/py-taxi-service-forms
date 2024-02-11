90c33658a87e4c3703e6f1efecbe51ff1f1a61ec
Volodymyr Kovalenko <380974261314kv@gmail.com>
2024-02-11 23:44:06 +0200

    taxi_service 'form' fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("accounts/logout/", views.logout_view),
     path(
         "manufacturers/",
         views.ManufacturerListView.as_view(),
