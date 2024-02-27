faa7c3ae190f1b30b2f778853bba2b9fcc716f41
barabarinov <barabarinov@gmail.com>
2024-02-27 12:12:10 +0200

    fixed

--- taxi/urls.py
+++ taxi/urls.py
@@ -43,7 +43,9 @@ urlpatterns = [
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path(
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
 ]
 
 app_name = "taxi"
