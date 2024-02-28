ba31308d832e7121d8bb6bfe6c925c0d84bfca0d
Ant <u123@ua.fm>
2024-02-28 22:33:20 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -64,7 +64,9 @@ class CarCreateView(LoginRequiredMixin, generic.CreateView):
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     fields = "__all__"
-    success_url = reverse_lazy("taxi:car-list")
+
+    def get_success_url(self):
+        return reverse("taxi:car-detail", kwargs={"pk": self.kwargs["pk"]})
 
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
