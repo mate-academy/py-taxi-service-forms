7620bc4325aa68164994d4b6e70076123c7b84e1
Sikach Illia <sikatch.ilja@gmail.com>
2024-02-29 18:47:10 +0200

    -added blank lines in html -flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -31,9 +31,12 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("manufacturer/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("manufacturer/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
-    path("manufacturer/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path("manufacturer/create/", ManufacturerCreateView.as_view(),
+         name="manufacturer-create"),
+    path("manufacturer/<int:pk>/update/", ManufacturerUpdateView.as_view(),
+         name="manufacturer-update"),
+    path("manufacturer/<int:pk>/delete/", ManufacturerDeleteView.as_view(),
+         name="manufacturer-delete"),
 ]
 
 app_name = "taxi"
