df9acadc5b3022ba4bb8f2eae5f454626e6843c6
Nick Illich <nickillich96@gmail.com>
2024-03-05 05:44:12 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -60,36 +60,32 @@ class DriverDetailView(LoginRequiredMixin, generic.DetailView):
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_create.html"
+    template_name = "taxi/car_form.html"
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     form_class = CarForm
-    template_name = "taxi/car_create.html"
     success_url = reverse_lazy("taxi:car-list")
 
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
-    template_name = "taxi/car_confirm_delete.html"
     success_url = reverse_lazy("taxi:car-list")
 
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     form_class = ManufacturerForm
-    template_name = "taxi/manufacturer_create.html"
+    template_name = "taxi/manufacturer_form.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Manufacturer
     form_class = ManufacturerForm
-    template_name = "taxi/manufacturer_create.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_confirm_delete.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
