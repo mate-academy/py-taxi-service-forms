b38bd58770aae0e705b50a485c02eea26cd8cdac
Andrii Zubchenko <dissom1987@gmail.com>
2024-04-01 13:43:04 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.views import generic
 
 from .models import Driver, Car, Manufacturer
 
