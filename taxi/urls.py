1d08a5f4a0dd7828e4c96e6d36f5dce456039f25
Vitalii <vitalikbashkiser@gmail.com>
2024-05-14 14:36:19 +0300

    Solution#2

--- taxi/urls.py
+++ taxi/urls.py
@@ -12,7 +12,7 @@ from taxi.views import (
     CarDeleteView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
@@ -22,55 +22,29 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "cars/",
-        CarListView.as_view(),
-        name="car-list"
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path(
-        "cars/<int:pk>/",
-        CarDetailView.as_view(),
-        name="car-detail"
-    ),
-    path(
-        "drivers/",
-        DriverListView.as_view(),
-        name="driver-list"
-    ),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"
-    ),
-    path(
-        "car/create/",
-        CarCreateView.as_view(),
-        name="car-create"
-    ),
-    path(
-        "car/<int:pk>/update/",
-        CarUpdateView.as_view(),
-        name="car-update"
-    ),
-    path(
-        "car/<int:pk>/delete/",
-        CarDeleteView.as_view(),
-        name="car-delete"
-    ),
+    path("car/create/", CarCreateView.as_view(), name="car-create"),
+    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path(
         "manufacturer/create/",
         ManufacturerCreateView.as_view(),
-        name="manufacturer-create"
+        name="manufacturer-create",
     ),
     path(
         "manufacturer/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
-        name="manufacturer-update"
+        name="manufacturer-update",
     ),
     path(
         "manufacturer/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete"
+        name="manufacturer-delete",
     ),
 ]
 
