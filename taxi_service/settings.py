12d2d00fda14fe90a1247a89d4e910980f0d1f87
Artem Yatsuta <yatsutaproject@gmail.com>
2024-06-18 13:05:28 +0200

    bootstrap again

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "taxi",
     "crispy_forms",
-    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -143,5 +142,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "uni_form"
