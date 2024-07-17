82acb2d2e17229cdbfa7073df6990744e6b03c79
Vlad <begavlad068@gmail.com>
2024-07-17 13:51:06 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -5,6 +5,8 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
+
+
 @login_required
 def index(request):
     """View function for the home page of the site."""
@@ -20,6 +22,8 @@ def index(request):
         "num_visits": num_visits + 1,
     }
     return render(request, "taxi/index.html", context=context)
+
+
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
@@ -51,6 +55,8 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.all().select_related("manufacturer")
+
+
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
 
@@ -78,6 +84,8 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
+
+
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
