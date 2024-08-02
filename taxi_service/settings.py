1076024b9e34faf32bf20a14daf1c8452f871b66
Ira <iramladanovych@gmail.com>
2024-08-02 10:14:38 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -142,5 +142,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 CRISPY_TEMPLATE_PACK = "bootstrap4"
