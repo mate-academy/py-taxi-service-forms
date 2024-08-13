f8de9f80939f7a64a1fe7c59a27272f536882e8d
Юрий <you@exampleabc1234567892777@gmail.com>
2024-08-13 21:45:29 +0300

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
