468a121bd13644f1024506f5283be11569f77a6f
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-08-17 07:45:39 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
@@ -11,18 +12,19 @@ class Manufacturer(models.Model):
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.name} {self.country}"
+        return f"{self.name}, {self.country}"
 
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
+        ordering = ("username",)
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
     def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
+        return f"{self.username}: ({self.first_name} {self.last_name})"
 
     def get_absolute_url(self):
         return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
@@ -31,7 +33,12 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="cars"
+    )
+
+    class Meta:
+        ordering = ("model",)
 
     def __str__(self):
-        return self.model
+        return f"{self.model}, {self.manufacturer.country}"
