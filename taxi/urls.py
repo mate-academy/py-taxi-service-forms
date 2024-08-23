d82c54e176f3caa04e09c6ad3a098358b80448c3
Maksym Protsak <maksym.protsak@gmail.com>
2024-08-23 13:32:52 +0300

    Solution 29-08-2021 13-32

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,6 +4,8 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarCreateView,
+    CarUpdateView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
@@ -18,6 +20,9 @@ urlpatterns = [
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/update/<int:pk>", CarUpdateView.as_view(), name="car-update"),
+
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
@@ -42,6 +43,20 @@ class CarListView(LoginRequiredMixin, generic.ListView):
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
+
+
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "core/car_form.html"
+
+
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "core/car_form.html"
 
 
 class DriverListView(LoginRequiredMixin, generic.ListView):
