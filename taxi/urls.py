1ecabae37a9509b674b2cdeaa5171c0594b312b4
Danyil <dartomov@gmail.com>
2024-09-11 11:32:31 +0200

    add missing '/' to urls and del reduntant method 'all()'

--- taxi/urls.py
+++ taxi/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
         name="manufacturer-update"
     ),
     path(
-        "manufacturers/<int:pk>/delete",
+        "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete"
     ),
@@ -41,7 +41,7 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete", CarDeleteView.as_view(), name="car-delete"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
