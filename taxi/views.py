46b767bf557d96b8bfb78716e7d2306379a9605d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-13 23:20:09 +0300

    fix requirements

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,4 @@
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
