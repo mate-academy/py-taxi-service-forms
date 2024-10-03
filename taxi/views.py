3b8144a3d891b1afdc5123a6d3df020023de48f2
Vlad Rymarchuk <vladrymarchuk@gmail.com>
2024-10-03 16:38:55 +0300

    Implemented 'Update' for manufacturers. Created link in taxi/urls.py. Also created class 'ManufacturerUpdateView' in taxi/views.py

--- taxi/views.py
+++ taxi/views.py
@@ -40,6 +40,12 @@ class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
+class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
