cf995bc5d16795826dfa7f172f94e9c1834426db
Frezworx <frezworx@gmail.com>
2024-11-09 12:22:36 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,15 +7,36 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ManufacturerCreateView,
+    ManufacturerUpdateView,
+    ManufacturerDeleteView, CarCreateView, CarUpdateView, CarDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturers-create",
+    ),
+
+    path("manufacturers/update/<int:pk>/",
+         ManufacturerUpdateView.as_view(),
+         name="manufacturers-update"),
+
+    path("manufacturers/delete/<int:pk>/",
+         ManufacturerDeleteView.as_view(),
+         name="manufacturers-delete"),
+
+    path(
         "manufacturers/",
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+
+    path("cars/create/", CarCreateView.as_view(), name="cars-create"),
+    path("cars/update/<int:pk>/", CarUpdateView.as_view(), name="cars-update"),
+    path("cars/delete/<int:pk>/", CarDeleteView.as_view(), name="cars-delete"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,14 @@
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Driver, Car, Manufacturer
+from .models import (
+    Driver,
+    Car,
+    Manufacturer,
+)
 
 
 @login_required
@@ -27,11 +32,53 @@ def index(request):
     return render(request, "taxi/index.html", context=context)
 
 
+class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Manufacturer
+    fields = "__all__"
+    template_name = "taxi/manufacturer_create_or_update.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    template_name = "taxi/manufacturer_create_or_update.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_confirm_delete.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
+
+
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = ["model", "manufacturer", ]
+    template_name = "taxi/car_create_or_update.html"
+    success_url = reverse_lazy("taxi:car-list")
+
+
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = ["model", "manufacturer", ]
+    template_name = "taxi/car_create_or_update.html"
+
+    def get_success_url(self):
+        return reverse_lazy("taxi:car-detail", kwargs={"pk": self.object.pk})
+
+
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    template_name = "taxi/car_confirm_delete.html"
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class CarListView(LoginRequiredMixin, generic.ListView):
