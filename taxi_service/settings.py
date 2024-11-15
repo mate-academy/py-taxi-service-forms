2546996c54477fe047c17a4857183d44a5ea8733
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-11-15 19:39:36 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "taxi",
     "crispy_forms",
     "crispy_bootstrap4",
@@ -50,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
