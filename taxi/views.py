32c3dd94d4fc46b8f476b06bf59b2bf1a4e31d42
Bondzik-S <bondziks@gmail.com>
2024-11-21 20:00:36 +0200

    Solution[C

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,5 @@
+from django.urls import reverse_lazy
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
@@ -34,6 +36,26 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
 
+class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_form.html"
+
+
+class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_form.html"
+
+
+class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_confirm_delete.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
@@ -44,6 +66,26 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
 
 
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_form.html"
+
+
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_form.html"
+
+
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    template_name = "taxi/car_confirm_delete.html"
+    success_url = reverse_lazy("taxi:car-list")
+
+
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
@@ -52,3 +94,59 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+# def create_message_view(request: HttpRequest) -> HttpResponseRedirect:
+#     context = {}
+#     form = MessageForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return HttpResponseRedirect(reverse("messager:message-list"))
+#     context["form"] = form
+#     return render(request, "messager/message_form.html", context=context)
+
+
+
+    # if request.method == "GET":
+    #     context1 = {
+    #         "form": CarForm()
+    #     }
+    #     return render(request, "taxi/car_form.html", context=context1)
+    # elif request.method == "POST":
+    #     form = CarForm(request.POST)
+    #
+    #     if form.is_valid():
+    #         Car.objects.create(**form.cleaned_data)
+    #         return HttpResponseRedirect(reverse("taxi:car-list"))
+    #     context2 = {
+    #         "form": form
+    #     }
+    #     return render(request, "taxi/car_form.html", context=context2)
+
+
+# def manufacturer_create_view(request: HttpRequest) -> HttpResponseRedirect:
+#     context = {}
+#     form = ManufacturerForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return HttpResponseRedirect(reverse("taxi:manufacturer-list"))
+#     context["form"] = form
+#     return render(request, "taxi/manufacturer_form.html", context=context)
+
+
+    # if request.method == "GET":
+    #     context1 = {
+    #         "form": ManufacturerForm()
+    #     }
+    #     return render(request, "taxi/manufacturer_form.html", context=context1)
+    # elif request.method == "POST":
+    #     form = ManufacturerForm(request.POST)
+    #
+    #     if form.is_valid():
+    #         Manufacturer.objects.create(**form.cleaned_data)
+    #         return HttpResponseRedirect(reverse("taxi:manufacturer-list"))
+    #     context2 = {
+    #         "form": form
+    #     }
+    #     return render(request, "taxi/manufacturer_form.html", context=context2)
+
