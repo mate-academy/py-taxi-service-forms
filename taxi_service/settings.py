e76c550219debbfce5d8e1cf18a19fdc2cdbc61d
Dmitry <dimon032006@gmail.com>
2024-11-21 20:08:52 +0100

    update

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -12,9 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc"
-)
+SECRET_KEY = ("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = []
