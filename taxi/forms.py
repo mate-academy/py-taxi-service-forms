11e8d28aaf9515fdbb650b5a9624812877f6e4a5
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-11-25 20:13:15 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -3,22 +3,24 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from .models import Car, Manufacturer
 
+
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = ['model', 'manufacturer', 'drivers']
+        fields = ["model", "manufacturer", "drivers"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'Save'))
+        self.helper.add_input(Submit("submit", "Save"))
+
 
 class ManufacturerForm(forms.ModelForm):
     class Meta:
         model = Manufacturer
-        fields = ['name', 'country']
+        fields = ["name", "country"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'Save'))
+        self.helper.add_input(Submit("submit", "Save"))
--- taxi/views.py
+++ taxi/views.py
@@ -60,38 +60,38 @@ class DriverDetailView(LoginRequiredMixin, generic.DetailView):
 class CarCreateView(CreateView):
     model = Car
     form_class = CarForm
-    template_name = 'taxi/car_form.html'
-    success_url = reverse_lazy('taxi:car-list')
+    template_name = "taxi/car_form.html"
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarForm
-    template_name = 'taxi/car_form.html'
-    success_url = reverse_lazy('taxi:car-list')
+    template_name = "taxi/car_form.html"
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class CarDeleteView(DeleteView):
     model = Car
-    template_name = 'taxi/car_confirm_delete.html'
-    success_url = reverse_lazy('taxi:car-list')
+    template_name = "taxi/car_confirm_delete.html"
+    success_url = reverse_lazy("taxi:car-list")
 
 
 class ManufacturerCreateView(CreateView):
     model = Manufacturer
     form_class = ManufacturerForm
-    template_name = 'taxi/manufacturer_form.html'
-    success_url = reverse_lazy('taxi:manufacturer-list')
+    template_name = "taxi/manufacturer_form.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerUpdateView(UpdateView):
     model = Manufacturer
     form_class = ManufacturerForm
-    template_name = 'taxi/manufacturer_form.html'
-    success_url = reverse_lazy('taxi:manufacturer-list')
+    template_name = "taxi/manufacturer_form.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerDeleteView(DeleteView):
     model = Manufacturer
-    template_name = 'taxi/manufacturer_confirm_delete.html'
-    success_url = reverse_lazy('taxi:manufacturer-list')
+    template_name = "taxi/manufacturer_confirm_delete.html"
+    success_url = reverse_lazy("taxi:manufacturer-list")
