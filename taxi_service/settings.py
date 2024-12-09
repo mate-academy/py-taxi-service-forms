8bcb90bf4492f95e88969b8f2e04f37b02cd4f57
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-12-09 22:12:38 +0200

    installed crispy_bootstrap4

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -117,6 +117,8 @@ AUTH_USER_MODEL = "taxi.Driver"
 
 LOGIN_REDIRECT_URL = "/"
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -142,5 +144,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
