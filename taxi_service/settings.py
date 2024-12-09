5d19be5d17965fb1f15ee9da9f3aef295f6efd38
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-12-09 22:30:19 +0200

    fixed mistakes

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -79,6 +79,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "taxi_service.wsgi.application"
 
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.logging.LoggingPanel",
+    # "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
