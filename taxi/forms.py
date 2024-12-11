833afbb6a781aec7e5cbf1db7a3a8bd24f734fb8
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-12-11 21:07:13 +0200

    Solution-3,3

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,14 +1,15 @@
 from django import forms
-from .models import Car, Manufacturer
+
+from taxi.models import Car, Manufacturer
 
 
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = "__all__"  # Adjust fields based on my model
+        fields = "__all__"
 
 
 class ManufacturerForm(forms.ModelForm):
     class Meta:
         model = Manufacturer
-        fields = "__all__"  # Adjust fields based on my model
+        fields = "__all__"
