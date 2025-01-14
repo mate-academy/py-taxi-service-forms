a4e61f3831899f1d6698fe11c3adb66740e5e480
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-01-14 21:19:30 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -36,6 +36,7 @@ INTERNAL_IPS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "crispy_forms",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -140,3 +141,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
