dc5a538787147a49a9d1a6c60cc1bb4f7c889ca0
4ikkamony <compbanderloga@gmail.com>
2025-01-26 16:26:44 +0200

    ModelForms for Manufacturer and Car

---
+++ taxi/forms.py
@@ -0,0 +1,35 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django import forms
+
+from taxi.models import Manufacturer, Car
+
+class ManufacturerForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = ""
+        self.helper.form_method = "post"
+
+    class Meta:
+        model = Manufacturer
+        fields = ["name", "country"]
+        widgets = {
+        }
+
+
+class CarForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = ""
+        self.helper.form_method = "post"
+
+    class Meta:
+        model = Car
+        fields = ["model", "manufacturer", "drivers"]
+        widgets = {
+            "drivers": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-select drivers-field", "size": "5"}
+            )
+        }
