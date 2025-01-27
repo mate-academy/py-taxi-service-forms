0fd8bccbd2ffb811f9701cd3772d5d57abf9cc9c
git_practic <dogoberrr@gmail.com>
2025-01-27 16:49:31 +0200

    Solution

---
+++ taxi/views.py
@@ -0,0 +1,123 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Driver, Car, Manufacturer
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_drivers": num_drivers,
+        "num_cars": num_cars,
+        "num_manufacturers": num_manufacturers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(LoginRequiredMixin, generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    template_name = "taxi/manufacturer_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.request.session["current_page"] = self.request.GET.get("page", 1)
+        return context
+
+
+class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Manufacturer
+    fields = "__all__"
+    template_name = "taxi/taxi_form.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:manufacturer-list") + f"?page={current_page}"
+
+
+class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    template_name = "taxi/taxi_form.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:manufacturer-list") + f"?page={current_page}"
+
+
+class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_delete.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:manufacturer-list") + f"?page={current_page}"
+
+
+class CarListView(LoginRequiredMixin, generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.all().select_related("manufacturer")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.request.session["current_page"] = self.request.GET.get("page", 1)
+        return context
+
+
+class CarDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Car
+
+
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = "__all__"
+    template_name = "taxi/taxi_form.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:car-list") + f"?page={current_page}"
+
+
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = "__all__"
+    template_name = "taxi/taxi_form.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:car-list") + f"?page={current_page}"
+
+
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    template_name = "taxi/car_delete.html"
+
+    def get_success_url(self):
+        current_page = self.request.session.get("current_page", 1)
+        return reverse_lazy("taxi:car-list") + f"?page={current_page}"
+
+
+class DriverListView(LoginRequiredMixin, generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
