0e4a5191f117042b4ab5e393ade8e1abe03571a2
Julia Alexeeva <julia4406@gmail.com>
2025-01-27 12:05:25 +0200

    Added trailing slash to url-pattern 'cars/create/'

--- taxi/urls.py
+++ taxi/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
 
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create", CarCreateView.as_view(), name="car-create"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
 
