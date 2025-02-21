a6c202695fad3c288bf73d75bcd5bc590d1c6ce9
Anna Lyubinska <anna.lyubinskagmail.com>
2025-02-21 19:09:57 +0100

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import (
-    index,
-    CarListView,
-    CarDetailView,
-    DriverListView,
-    DriverDetailView,
-    ManufacturerListView,
-)
+from .views import (index, CarListView, CarDetailView,
+                    DriverListView, DriverDetailView, ManufacturerListView,
+                    CarCreateView, CarDeleteView, CarUpdateView,
+                    ManufacturerCreateView, ManufacturerDeleteView,
+                    ManufacturerUpdateView)
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -22,6 +20,15 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("car/create/", CarCreateView.as_view(), name="car-create"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("manufacturer/create/", ManufacturerCreateView.as_view(),
+         name="manufacturer-create"),
+    path("manufacturer/<int:pk>/delete/", ManufacturerDeleteView.as_view(),
+         name="manufacturer-delete"),
+    path("manufacturer/<int:pk>/update/", ManufacturerUpdateView.as_view(),
+         name="manufacturer-update"),
 ]
 
 app_name = "taxi"
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "debug_toolbar",
     "taxi",
 ]
@@ -140,3 +142,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
