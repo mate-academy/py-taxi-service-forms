69868c474d8fd81c2a74e455fa8f49b7ad6c51a4
Dolgov Dmytro <dimakherson74@gmail.com>
2025-02-27 13:27:51 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -70,7 +70,6 @@ class CarUpdateView(LoginRequiredMixin, UpdateView):
 
 class CarDeleteView(LoginRequiredMixin, DeleteView):
     model = Car
-    fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
     template_name = "taxi/car_confirm_delete.html"
 
@@ -89,6 +88,5 @@ class ManufacturerUpdateView(LoginRequiredMixin, UpdateView):
 
 class ManufacturerDeleteView(LoginRequiredMixin, DeleteView):
     model = Manufacturer
-    fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
     template_name = "taxi/manufacturer_confirm_delete.html"
