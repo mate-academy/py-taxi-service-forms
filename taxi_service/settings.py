2b399399536bf5d8b609cc24c96db82579fee32e
Aleksandr <sasha.syrota15@gmail.com>
2025-03-26 04:40:56 +0200

    fix cancell_button

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "taxi",
     "crispy_forms",
+    "crispy_bootstrap4",
+    "taxi",
+
 ]
 
 MIDDLEWARE = [
@@ -141,3 +143,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
