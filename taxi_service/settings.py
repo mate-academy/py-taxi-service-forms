20688dbf05046cb24c48a9df2efaf09f9c220c35
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-04-09 20:00:14 +0200

    'Solution'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -48,9 +48,6 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
@@ -145,3 +142,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
