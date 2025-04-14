06944da47db5d4282821b7fd42ba3ff845c1a1cc
Yaroslav Kalugin <yaroslav.kalugin.07@gmail.com>
2025-04-14 17:42:08 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -65,20 +65,20 @@ class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
     fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi:car_form.html"
+    template_name = "taxi/car_form.html"
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi:car_form.html"
+    template_name = "taxi/car_form.html"
 
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi:car_delete.html"
+    template_name = "taxi/car_delete.html"
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
