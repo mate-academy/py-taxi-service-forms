36eb11f2616e87c64605038efb902b277eeeefa1
Atom-gnomov <gorbach0864213579@gmail.com>
2025-04-15 18:40:21 +0300

    complete

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,8 +6,13 @@ from .views import (
     CarDetailView,
     DriverListView,
     DriverDetailView,
-    ManufacturerListView,
+    ManufacturerListView, CarCreateView, CarUpdateView, CarDeleteView, ManufacturerCreateView, ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
+
+
+
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -22,6 +27,14 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+
+
 ]
 
 app_name = "taxi"
