291e218ac839b20f8fac5135a375e8962375101c
Olha Shumska <olgashumska65@gmail.com>
2025-06-11 21:40:11 +0200

    fields specified

--- taxi/views.py
+++ taxi/views.py
@@ -37,14 +37,14 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
-    fields = "__all__"
+    fields = ["name", "country", ]
     template_name = "taxi/manufacturer_form.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
 
 class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Manufacturer
-    fields = "__all__"
+    fields = ["name", "country", ]
     template_name = "taxi/manufacturer_form.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
@@ -67,7 +67,7 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    fields = ["model", "manufacturer", "drivers", ]
     success_url = reverse_lazy("taxi:car-list")
     template_name = "taxi/car_form.html"
 
@@ -80,7 +80,7 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    fields = "__all__"
+    fields = ["model", "manufacturer", "drivers", ]
     success_url = reverse_lazy("taxi:car-list")
     template_name = "taxi/car_form.html"
 
