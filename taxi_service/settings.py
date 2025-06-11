a993e999a2a92157657b2347077737bf35b849c3
Olha Shumska <olgashumska65@gmail.com>
2025-06-11 18:46:33 +0200

    falke8 taxi_service

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CRISPY_TEMPLATE_PACK="bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
