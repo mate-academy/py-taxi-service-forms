63f9550eea9eaa2915479265eface624631f7b2f
Polevoy Yevhen <polevoye90@gmail.ua>
2025-06-12 16:19:40 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -55,25 +55,25 @@ class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
 
 
-class CarListCreateView(LoginRequiredMixin, generic.CreateView):
+class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
     fields = "__all__"
-    success_url = reverse_lazy("taxi:car-list")
+    success_url = reverse_lazy("taxi:car-create")
     template_name = "taxi/car_form.html"
 
 
-class CarListUpdateView(LoginRequiredMixin, generic.UpdateView):
+class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
     fields = "__all__"
-    success_url = reverse_lazy("taxi:car-list")
+    success_url = reverse_lazy("taxi:car-update")
     template_name = "taxi/car_form.html"
 
 
-class CarListDeleteView(LoginRequiredMixin, generic.DeleteView):
+class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     # fields = "__all__"
     template_name = "taxi/car_confirm_delete.html"
-    success_url = reverse_lazy("taxi:car-list")
+    success_url = reverse_lazy("taxi:car-delete")
 
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
