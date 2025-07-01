5ca488380cd2c93fea5b48333ff2a7f0e426c417
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-01 16:22:26 +0300

    add missing trailing slashes at the URL patterns for car update and delete

--- taxi/urls.py
+++ taxi/urls.py
@@ -41,8 +41,8 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("car/create/", CreateCarView.as_view(), name="car-create"),
-    path("car/<int:pk>/update", CarUpdateView.as_view(), name="car-update"),
-    path("car/<int:pk>/delete", CarDeleteView.as_view(), name="car-delete"),
+    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
