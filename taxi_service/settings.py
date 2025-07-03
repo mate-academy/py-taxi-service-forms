d9548c520d76eceef3b4c8fed60c3aeef7f64df7
yuraf <twinkreferal22847228@gmail.com>
2025-07-03 19:34:43 +0300

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "taxi",
-    "crispy_bootstrap4",
-    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -142,7 +140,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
