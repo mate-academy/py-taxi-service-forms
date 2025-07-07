59111a062672793d35cfe6838b20bf5d9a81fe40
Sasha <kluzodota@gmail.com>
2025-07-07 18:51:40 +0300

    seventh fix

--- taxi/views.py
+++ taxi/views.py
@@ -62,37 +62,37 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     def get_success_url(self):
         return reverse_lazy("taxi:car-detail", kwargs={"pk": self.object.pk})
 
-    template_name = "taxi/car_form.html/"
+    template_name = "taxi/car_form.html"
 
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
     fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_form.html/"
+    template_name = "taxi/car_form.html"
 
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_delete.html/"
+    template_name = "taxi/car_delete.html"
 
 
 class ManufacturerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Manufacturer
     fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_form.html/"
+    template_name = "taxi/manufacturer_form.html"
 
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
     fields = "__all__"
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_form.html/"
+    template_name = "taxi/manufacturer_form.html"
 
 
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_delete.html/"
+    template_name = "taxi/manufacturer_delete.html"
