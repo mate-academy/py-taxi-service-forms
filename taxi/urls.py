8ba6e1a3e219364c2cf85fd8ad5c9730392fdfa4
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-08-08 19:31:49 +0200

    taxi-service form

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,52 +3,79 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarCreateView,
+    CarUpdateView,
+    CarDeleteView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
-    CarCreateView,
-    CarUpdateView,
-    CarDeleteView,
 )
 
 app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
+
+    # Manufacturer URLs
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
-        name="manufacturer-list"
+        name="manufacturer-list",
     ),
     path(
-        "manufacturers/create",
+        "manufacturers/create/",
         ManufacturerCreateView.as_view(),
-        name="manufacturer-create"
+        name="manufacturer-create",
     ),
     path(
-        "manufacturers/<int:pk>/update/",
+        "manufacturers/<int:pk>/",
         ManufacturerUpdateView.as_view(),
-        name="manufacturer-update"
+        name="manufacturer-update",
     ),
     path(
         "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete"
+        name="manufacturer-delete",
     ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(),
-         name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(),
-         name="car-delete"),
-    path("cars/<int:pk>/", CarDetailView.as_view(),
-         name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
+
+    # Car URLs
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(),
-        name="driver-detail"
+        "cars/",
+        CarListView.as_view(),
+        name="car-list",
+    ),
+    path(
+        "cars/create/",
+        CarCreateView.as_view(),
+        name="car-create",
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail",
+    ),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-update",
+    ),
+    path(
+        "cars/<int:pk>/delete/",
+        CarDeleteView.as_view(),
+        name="car-delete",
+    ),
+
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list",
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail",
     ),
 ]
