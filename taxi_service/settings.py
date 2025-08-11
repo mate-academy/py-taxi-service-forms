2a4ff4ea8cd138185263d813cd30fe27e6e01acf
Andriy125 <yaruta.andriy@gmail.com>
2025-08-11 16:23:21 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "taxi",
+    # "crispy_bootstrap4",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "taxi_service.urls"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS="bootstrap4"
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
 
 TEMPLATES = [
     {
