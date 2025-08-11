fdb7bc6ecb5661735abfc5186089ffd6a9b35fea
Andriy125 <yaruta.andriy@gmail.com>
2025-08-11 16:37:42 +0300

    Style: fix error flake8 E225 on line 65,65

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -61,10 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "taxi_service.urls"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS="bootstrap4"
-
-CRISPY_TEMPLATE_PACK="bootstrap4"
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 TEMPLATES = [
     {
