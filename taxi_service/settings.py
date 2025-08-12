c6a732f45c7f353e32b5dca26e96d7ccc511d02b
Farzam <farzamgt@gmail.com>
2025-08-12 22:21:15 +0300

    crispy_bootstrap4 was added

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
