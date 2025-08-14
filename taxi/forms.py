9a97086c7d3783f6721408ab246a267794cbea36
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-08-14 19:11:34 +0300

    helper using implemented

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 
 from taxi.models import Car, Manufacturer
@@ -8,8 +10,20 @@ class CarForm(forms.ModelForm):
         model = Car
         fields = ["model", "manufacturer", "drivers"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Save"))
+
 
 class ManufacturerForm(forms.ModelForm):
     class Meta:
         model = Manufacturer
         fields = ["name", "country"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Save"))
