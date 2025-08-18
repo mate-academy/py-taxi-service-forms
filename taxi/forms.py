6ca66eb78d6a667dee8947f46c6cc47e5ffc9ab1
Maksym Buslenko <maksbusl@gmail.com>
2025-08-18 20:44:15 +0300

    fix with flake

--- taxi/forms.py
+++ taxi/forms.py
@@ -2,19 +2,24 @@ from crispy_forms.helper import FormHelper
 from django import forms
 from .models import Car, Manufacturer
 
+
 class CarForm(forms.ModelForm):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+
     class Meta:
         model = Car
         fields = "__all__"
 
 
 class ManufacturerForm(forms.ModelForm):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+
     class Meta:
         model = Manufacturer
         fields = "__all__"
