8568176ff01ab88ffd22e8a7b526121872da7dea
Maksym Buslenko <maksbusl@gmail.com>
2025-08-18 20:24:56 +0300

    fix cancel, urls and requirement.txt

--- taxi/urls.py
+++ taxi/urls.py
@@ -32,17 +32,17 @@ urlpatterns = [
     path("cars/<int:pk>/update", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete", CarDeleteView.as_view(), name="car-delete"),
     path(
-        "manufacturer/create/",
+        "manufacturers/create/",
         ManufacturerCreateView.as_view(),
         name="manufacturer-create"
     ),
     path(
-        "manufacturer/<int:pk>/update",
+        "manufacturers/<int:pk>/update",
         ManufacturerUpdateView.as_view(),
         name="manufacturer-update"
     ),
     path(
-        "manufacturer/<int:pk>/delete",
+        "manufacturers/<int:pk>/delete",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete"
     ),
