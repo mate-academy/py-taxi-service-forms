66b40f79eeb091f3d06f5675860416fc6fe9e8c6
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-02 18:48:09 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,6 +7,12 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    CarFormatCreateView,
+    CarFormatUpdateView,
+    CarFormatDeleteView,
+    ManufacturerFormatCreateView,
+    ManufacturerFormatUpdateView,
+    ManufacturerFormatDeleteView,
 )
 
 urlpatterns = [
@@ -16,8 +22,38 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path(
+        "manufacturers/create/",
+        ManufacturerFormatCreateView.as_view(),
+        name="manufacturer-format-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerFormatUpdateView.as_view(),
+        name="manufacturer-format-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerFormatDeleteView.as_view(),
+        name="manufacturer-format-delete",
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path(
+        "cars/create/",
+        CarFormatCreateView.as_view(),
+        name="car-format-create"
+    ),
+    path(
+        "cars/<int:pk>/update/",
+        CarFormatUpdateView.as_view(),
+        name="car-format-update"
+    ),
+    path(
+        "cars/<int:pk>/delete/",
+        CarFormatDeleteView.as_view(),
+        name="car-format-delete"
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -34,10 +35,52 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
 
+class ManufacturerFormatCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_form.html"
+
+
+class ManufacturerFormatUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_form.html"
+
+
+class ManufacturerFormatDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Manufacturer
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+    template_name = "taxi/manufacturer_confirm_delete.html"
+
+
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.all().select_related("manufacturer")
+
+
+class CarFormatCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_form.html"
+
+
+class CarFormatUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_form.html"
+
+
+class CarFormatDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Car
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:car-list")
+    template_name = "taxi/car_confirm_delete.html"
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
