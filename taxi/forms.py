6c875bb7e214e27cd8b5b7b409d33f658868b003
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-09-20 21:35:45 +0200

    Solution

---
+++ taxi/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Car, Manufacturer, Driver
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = ["model", "manufacturer", "drivers"]
+        widgets = {
+            "model": forms.TextInput(attrs={"class": "form-control"}),
+            "manufacturer": forms.Select(attrs={"class": "form-control"}),
+            "drivers": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["manufacturer"].queryset = Manufacturer.objects.all()
+        self.fields["drivers"].queryset = Driver.objects.all()
+        self.fields["drivers"].required = False
+
+
+class ManufacturerForm(forms.ModelForm):
+    class Meta:
+        model = Manufacturer
+        fields = ["name", "country"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "country": forms.TextInput(attrs={"class": "form-control"}),
+        }
--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 
 from .views import (
     index,
@@ -22,6 +23,12 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("cars/create/", views.CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", views.CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", views.CarDeleteView.as_view(), name="car-delete"),
+    path("manufacturers/create/", views.ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturers/<int:pk>/update/", views.ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturers/<int:pk>/delete/", views.ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
 ]
 
 app_name = "taxi"
