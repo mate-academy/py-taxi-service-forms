8afc8ace2a4a4a3de63ae07ed0fe2113712de4c7
Taras <tarasostapchuk27@gmail.com>
2025-09-28 00:04:08 +0200

    Solution Correct 3

--- taxi/forms.py
+++ taxi/forms.py
@@ -4,10 +4,12 @@ from taxi.models import Car, Manufacturer
 
 
 class CarForm(forms.ModelForm):
-    model = Car
-    fields = "__all__"
+    class Meta:
+        model = Car
+        fields = "__all__"
 
 
 class ManufacturerForm(forms.ModelForm):
-    model = Manufacturer
-    fields = "__all__"
+    class Meta:
+        model = Manufacturer
+        fields = "__all__"
