7358770ccf4347a9643cda35e0749b8ab4b03465
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-10-15 17:58:18 -0300

    solution 4

--- taxi/forms.py
+++ taxi/forms.py
@@ -8,7 +8,10 @@ class CarForm(forms.ModelForm):
         fields = ["model", "manufacturer", "drivers"]
 
         widgets = {
-            "model": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter car model"}),
+            "model": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter car model"}),
             "manufacturer": forms.Select(attrs={"class": "form-control"}),
             "drivers": forms.SelectMultiple(attrs={"class": "form-control"}),
         }
@@ -16,5 +19,6 @@ class CarForm(forms.ModelForm):
     def clean_model(self):
         model = self.cleaned_data["model"]
         if len(model) < 2:
-            raise forms.ValidationError("Model name must have at least 2 characters.")
+            raise forms.ValidationError(
+                "Model name must have at least 2 characters.")
         return model
