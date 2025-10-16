2f5949cb3a2d06ffa73dc13cabeee18bcc44605d
Tomasz Herman <tomekh2015@gmail.com>
2025-10-16 15:53:00 +0200

    Solution fixed taxi urls

--- taxi/urls.py
+++ taxi/urls.py
@@ -20,16 +20,8 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path(
-        "cars/<int:pk>/update/",
-        CarUpdateView.as_view(),
-        name="car-update",
-    ),
-    path(
-        "cars/<int:pk>/delete/",
-        CarDeleteView.as_view(),
-        name="car-delete",
-    ),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/",
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,53 +1,7 @@
-from django.urls import path
-
-from .views import (
-    index,
-    CarListView,
-    CarDetailView,
-    CarCreateView,
-    CarUpdateView,
-    CarDeleteView,
-    DriverListView,
-    DriverDetailView,
-    ManufacturerListView,
-    ManufacturerCreateView,
-    ManufacturerUpdateView,
-    ManufacturerDeleteView,
-)
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"
-    ),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path(
-        "manufacturers/create/",
-        ManufacturerCreateView.as_view(),
-        name="manufacturer-create",
-    ),
-    path(
-        "manufacturers/<int:pk>/update/",
-        ManufacturerUpdateView.as_view(),
-        name="manufacturer-update",
-    ),
-    path(
-        "manufacturers/<int:pk>/delete/",
-        ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete",
-    ),
+    path("admin/", admin.site.urls),
+    path("", include("taxi.urls", namespace="taxi")),
 ]
-
-app_name = "taxi"
