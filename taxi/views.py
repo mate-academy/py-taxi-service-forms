e70bb07b10c7ba462701b7ffa37a2c7cf1abef33
henriquejhansen <henriquejhansen@gmail.com>
2025-11-08 19:46:19 -0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -8,6 +8,7 @@ from django.views.generic import (
 )
 from django.urls import reverse_lazy
 from .models import Car, Driver, Manufacturer
+from .forms import CarForm, ManufacturerForm
 
 
 class IndexView(TemplateView):
@@ -16,69 +17,53 @@ class IndexView(TemplateView):
 
 class CarListView(ListView):
     model = Car
-    template_name = "taxi/car_list.html"
-    context_object_name = "car_list"
 
 
 class CarDetailView(DetailView):
     model = Car
-    template_name = "taxi/car_detail.html"
-    context_object_name = "car"
 
 
 class CarCreateView(CreateView):
     model = Car
-    fields = "__all__"
-    template_name = "taxi/car_form.html"
+    form_class = CarForm
     success_url = reverse_lazy("car-list")
 
 
 class CarUpdateView(UpdateView):
     model = Car
-    fields = "__all__"
-    template_name = "taxi/car_form.html"
+    form_class = CarForm
     success_url = reverse_lazy("car-list")
 
 
 class CarDeleteView(DeleteView):
     model = Car
-    template_name = "taxi/car_confirm_delete.html"
     success_url = reverse_lazy("car-list")
 
 
 class ManufacturerListView(ListView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_list.html"
-    context_object_name = "manufacturer_list"
 
 
 class ManufacturerCreateView(CreateView):
     model = Manufacturer
-    fields = "__all__"
-    template_name = "taxi/manufacturer_form.html"
+    form_class = ManufacturerForm
     success_url = reverse_lazy("manufacturer-list")
 
 
 class ManufacturerUpdateView(UpdateView):
     model = Manufacturer
-    fields = "__all__"
-    template_name = "taxi/manufacturer_form.html"
+    form_class = ManufacturerForm
     success_url = reverse_lazy("manufacturer-list")
 
 
 class ManufacturerDeleteView(DeleteView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_confirm_delete.html"
     success_url = reverse_lazy("manufacturer-list")
 
 
 class DriverListView(ListView):
     model = Driver
-    template_name = "taxi/driver_list.html"
-    context_object_name = "driver_list"
 
 
 class DriverDetailView(DetailView):
     model = Driver
-    template_name = "taxi/driver_detail.html"
-    context_object_name = "driver"
