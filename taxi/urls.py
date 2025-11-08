a546d3dfcccea0ff5aaf96df8328ac6432c2f5c3
henriquejhansen <henriquejhansen@gmail.com>
2025-11-08 19:05:58 -0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,10 +11,34 @@ from taxi.views import (
 app_name = "taxi"
 
 urlpatterns = [
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
-    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path(
+        "cars/create/",
+        CarCreateView.as_view(),
+        name="car-create",
+    ),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-update",
+    ),
+    path(
+        "cars/<int:pk>/delete/",
+        CarDeleteView.as_view(),
+        name="car-delete",
+    ),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
 ]
