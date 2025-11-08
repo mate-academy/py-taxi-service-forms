f0f49de11475c44bdb9a0adc58e7bf2a49295030
henriquejhansen <henriquejhansen@gmail.com>
2025-11-08 17:53:10 -0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from .views import (
     index,
     CarListView,
@@ -8,20 +7,30 @@ from .views import (
     DriverDetailView,
     ManufacturerListView,
 )
+from taxi_service.views import (
+    CarCreateView, CarUpdateView, CarDeleteView,
+    ManufacturerCreateView, ManufacturerUpdateView, ManufacturerDeleteView
+)
+
+app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
+
+    # Manufacturer URLs
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+
+    # Car URLs
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
-]
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
 
-app_name = "taxi"
+    # Driver URLs
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+]
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -62,7 +62,8 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "taxi_service" / "templates"],
+
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
