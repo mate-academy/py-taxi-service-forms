fb4885359e6764b439acda6dce8edf0bc798c9a1
Serhii Chumak <s4umak@gmail.com>
2025-11-10 17:03:57 +0200

    Solution 3 py taxi service form

--- taxi/urls.py
+++ taxi/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
         name="manufacturer-create",
     ),
     path(
-        "manufacturers/update/",
+        "manufacturers/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
         name="manufacturer-update",
     ),
@@ -39,7 +39,7 @@ urlpatterns = [
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
