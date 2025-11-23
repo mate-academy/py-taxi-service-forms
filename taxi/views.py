bd9d03c390d333792ffd95239a1e85fe950d3ee1
Anastasiia Pohonets <29karasik@gmail.com>
2025-11-23 15:30:21 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
 
 from .models import Driver, Car, Manufacturer
 
