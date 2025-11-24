343b9577aa97dfa7447b5f7e3f864aeac8735238
Thaís <parisethais@gmail.com>
2025-11-23 21:23:18 -0300

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,6 @@
-from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -76,8 +76,6 @@ TEMPLATES = [
         },
     },
 ]
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "taxi_service.wsgi.application"
 
