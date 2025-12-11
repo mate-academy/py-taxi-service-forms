7ccdbb0c50c3847528e68527510d509ed19ac0cd
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-12-11 15:45:07 +0200

    Implemented changes

--- taxi/urls.py
+++ taxi/urls.py
@@ -29,13 +29,13 @@ urlpatterns = [
         name="manufacturer-update",
     ),
     path(
-        "manufacturers/<int:pk>/delete",
+        "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
 
