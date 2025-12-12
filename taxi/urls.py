e6fa3bf66715f1a578cec558ddb6511cd2b5a3d0
Vadym Vynokur <king.krollie@gmail.com>
2025-12-12 22:13:56 +0200

    add sol

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,7 +6,8 @@ from .views import (
     CarDetailView,
     DriverListView,
     DriverDetailView,
-    ManufacturerListView,
+    ManufacturerListView, CarCreateView, CarUpdateView, CarDeleteView, ManufacturerCreateView, ManufacturerUpdateView,
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
@@ -16,8 +17,15 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
+    path("manufactures/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufactures/<int:pk>/update", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufactures/<int:pk>/delete", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
